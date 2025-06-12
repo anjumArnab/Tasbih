6f51442584f3fe4110e1f5c66a5af017ec33898f
unknown <anjumarnab050@gmail.com>
2025-06-12 23:34:59 +0600

    I:

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import '../main.dart';
+import 'package:tasbih/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
