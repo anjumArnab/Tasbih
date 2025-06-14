916785e81c16ae1c8f35160606b91235d5563882
unknown <anjumarnab050@gmail.com>
2025-06-14 22:18:27 +0600

    I:

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import '../screens/homepage.dart';
-import 'models/dhikr.dart';
-import 'services/db_service.dart';
+import '../models/dhikr.dart';
+import '../services/db_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +30,7 @@ class Tasbih extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87),
         useMaterial3: true,
       ),
+
       home: const Homepage(),
     );
   }
