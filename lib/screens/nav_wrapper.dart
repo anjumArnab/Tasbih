f3859f63953011a99cb1654093128fce6205c847
unknown <anjumarnab050@gmail.com>
2025-06-26 19:49:42 +0600

    bottom navigation wrapper

---
+++ lib/screens/nav_wrapper.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import '../models/dhikr.dart';
+import '../screens/dhikr_achievement_page.dart';
+import '../screens/homepage.dart';
+import '../screens/dhikrpage.dart';
+
+class NavigationWrapper extends StatefulWidget {
+  final int initialIndex;
+  final Dhikr? selectedDhikr;
+
+  const NavigationWrapper({
+    super.key,
+    this.initialIndex = 0,
+    this.selectedDhikr,
+  });
+
+  @override
+  State<NavigationWrapper> createState() => _NavigationWrapperState();
+}
+
+class _NavigationWrapperState extends State<NavigationWrapper> {
+  late int _currentIndex;
+  late PageController _pageController;
+
+  static const Color primaryColor = Color(0xFF0F4C75);
+  static const Color surfaceColor = Colors.white;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.initialIndex;
+    _pageController = PageController(initialPage: _currentIndex);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    _pageController.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: [
+          Homepage(selectedDhikr: widget.selectedDhikr),
+          const Dhikrpage(),
+          const DhikrAchievementsPage(),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: surfaceColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 10,
+              offset: const Offset(0, -2),
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: _onTabTapped,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: surfaceColor,
+          selectedItemColor: primaryColor,
+          unselectedItemColor: Colors.grey[600],
+          selectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 12,
+          ),
+          unselectedLabelStyle: const TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 11,
+          ),
+          elevation: 0,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.auto_awesome_outlined),
+              activeIcon: Icon(Icons.auto_awesome),
+              label: 'Dhikr',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.emoji_events_outlined),
+              activeIcon: Icon(Icons.emoji_events),
+              label: 'Achievements',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
