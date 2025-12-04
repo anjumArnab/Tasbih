7c8c79aac555dcb2bd4da397b3cf9ab75b927837
Sakib Anjum Arnab <anjumarnab050@gmail.com>
2025-12-04 22:33:36 +0600

    new implementation for showActivityStatus()

--- lib/widgets/app_snack_bar.dart
+++ lib/widgets/app_snack_bar.dart
@@ -25,17 +25,38 @@ class AppSnackbar {
     _show(context, message, Colors.blue, durationInSeconds);
   }
 
+  static void showActivityStatus(
+    BuildContext context,
+    String message, {
+    int durationInSeconds = 3,
+  }) {
+    _show(
+      context,
+      message,
+      Color(0xFF0F4C75),
+      durationInSeconds,
+      behavior: SnackBarBehavior.floating,
+      shapeBorder: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
+
   static void _show(
     BuildContext context,
     String message,
     Color backgroundColor,
-    int durationInSeconds,
-  ) {
+    int durationInSeconds, {
+    SnackBarBehavior? behavior,
+    ShapeBorder? shapeBorder,
+  }) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(message),
         backgroundColor: backgroundColor,
         duration: Duration(seconds: durationInSeconds),
+        behavior: behavior,
+        shape: shapeBorder,
       ),
     );
   }
